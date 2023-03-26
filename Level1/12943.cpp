70f21d633713b14eca237486af1921e819722642
Klay <clay9502@naver.com>
2023-03-26 21:01:48 +0900

    Create 12943.cpp

---
+++ Level1/12943.cpp
@@ -0,0 +1,22 @@
+// 콜라츠 추
+// Programmers Link : https://school.programmers.co.kr/learn/courses/30/lessons/12943
+// Notion Link : https://www.notion.so/bf1c1075eafc476fa68523c2ed2dffc5?pvs=4
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    
+    while (answer <= 500 && n != 1)
+    {
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n = n * 3 + 1;
+        
+        answer++;
+    }
+    
+    return n == 1 ? answer : -1;
+}
