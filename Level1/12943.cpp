2836df40dafb2d4a7a1afc13a4215416334c7525
Klay <clay9502@naver.com>
2023-03-26 21:02:37 +0900

    Fix Comment Title Name

--- Level1/12943.cpp
+++ Level1/12943.cpp
@@ -1,4 +1,4 @@
-// 콜라츠 추
+// 콜라츠 추측
 // Programmers Link : https://school.programmers.co.kr/learn/courses/30/lessons/12943
 // Notion Link : https://www.notion.so/bf1c1075eafc476fa68523c2ed2dffc5?pvs=4
 
